1f540c1e25edff548a5ed8778aab26b659896b8d
keprydesousa <khepry.coder@gmail.com>
2022-07-15 15:43:20 -0300

    Update tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,8 @@
+"tem como sua principal diferença da tupla para lista é"
+"Ele não pode modificar os dados de dentro"
+
+v = (1,3,4,55)#tupla
+print(type(v)), print(v)
+
+v1 = 1 ,3 ,45 ,6; "não obrigatoriamente deve ter parenteses"
+print(type(v1))
